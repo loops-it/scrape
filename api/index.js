660074347dc62e32eeb2009b910b018e00e461d0
madushan10 <mnmnjith@gmail.com>
2025-04-03 17:22:54 +0530

    scrap update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-import puppeteer from 'puppeteer-core'; 
+import puppeteer from 'puppeteer';
 import express from 'express';
 import cors from 'cors';
-import chromium from 'chrome-aws-lambda';
 
 const port = 3004;
 const app = express();
 
 app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("server is working");
@@ -14,59 +14,56 @@ app.get("/", (req, res) => {
 
 app.post("/api/scrap-website", async (req, res) => {
   try {
-    const browser = await puppeteer.launch({
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'], // Add sandbox arguments
-      defaultViewport: chromium.defaultViewport,
-    });
-
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64)');
-    await page.goto('https://projects.worldbank.org/en/projects-operations/project-detail/P163742', {
-        waitUntil: 'networkidle2'
-    });
-
-    await page.waitForSelector('.intro_paragraph ._loop_lead_paragraph_sm'); 
-
-    const clickableElement = await page.$('.intro_paragraph ._loop_lead_paragraph_sm a.ng-tns-c2-0');
-    if (clickableElement) {
-        await page.evaluate((el) => el.scrollIntoView(), clickableElement); 
-        await clickableElement.click();
-    } else {
-        console.error('Element not found or not clickable');
-        await browser.close();
-        return;
-    }
-
-    await page.evaluate(() => {
-        const styles = `
-            .ng-trigger{
-                display: block !important;
-            }
-        `;
-        const styleSheet = document.createElement("style");
-        styleSheet.type = "text/css";
-        styleSheet.innerText = styles;
-        document.head.appendChild(styleSheet);
-        const unwantedSelectors = ['header', 'footer', 'nav', '.sidebar', '.ads', '.breadcrumbs', '.breadcrumb-section', '#f03v1-social-sharing', '#newsletters'];
-        unwantedSelectors.forEach(selector => {
-            document.querySelectorAll(selector).forEach(el => el.remove());
+    const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+    
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64)');
+        await page.goto('https://projects.worldbank.org/en/projects-operations/project-detail/P163742', {
+            waitUntil: 'networkidle2'
         });
-    });
+    
+        await page.waitForSelector('.intro_paragraph ._loop_lead_paragraph_sm'); 
 
-    const mainContent = await page.evaluate(() => {
-        return document.body.innerText.trim();
-    });
+        const clickableElement = await page.$('.intro_paragraph ._loop_lead_paragraph_sm a.ng-tns-c2-0');
+        if (clickableElement) {
+            await page.evaluate((el) => el.scrollIntoView(), clickableElement); 
+            await clickableElement.click();
+        } else {
+            console.error('Element not found or not clickable');
+            await browser.close();
+            return;
+        }
 
-    console.log(mainContent); 
-    await browser.close(); 
-
-    if (mainContent) {
-        res.json({ content: mainContent });
-    } else {
-        res.status(500).send('No content extracted');
-    }
+        await page.evaluate(() => {
+            const styles = `
+                .ng-trigger{
+                    display: block !important;
+                }
+            `;
+    
+            const styleSheet = document.createElement("style");
+            styleSheet.type = "text/css";
+            styleSheet.innerText = styles;
+            document.head.appendChild(styleSheet);
+    
+            const unwantedSelectors = ['header', 'footer', 'nav', '.sidebar', '.ads', '.breadcrumbs', '.breadcrumb-section', '#f03v1-social-sharing', '#newsletters'];
+            unwantedSelectors.forEach(selector => {
+                document.querySelectorAll(selector).forEach(el => el.remove());
+            });
+        });
+    
+        const mainContent = await page.evaluate(() => {
+            return document.body.innerText.trim();
+        });
+    
+        console.log(mainContent); 
+        await browser.close(); 
+    
+        if (mainContent) {
+            res.json({ content: mainContent });
+        } else {
+            res.status(500).send('No content extracted');
+        }
 
   } catch (error) {
     console.error("Error processing file:", error);
